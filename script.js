47b7ed95b71da2295e3fef9f32b50604ece3ccb4
Ajeet Gupta <229371738+gajit9147-dev@users.noreply.github.com>
2026-02-02 00:41:08 +0530

    some more contant

--- script.js
+++ script.js
@@ -30,3 +30,25 @@ const multiply = (x, y) => {
     product = x * y
     console.log("The product of x and y is ", product)
 }
+multiply(5, 10)
+
+// beauty of arrow function when there is single line of code inside the function body we can remove the curly braces and return keyword
+const divide = (x, y) => x / y
+console.log("The division of x and y is ", divide(10, 5))
+
+// one more example of arrow function
+const greet = name => `Hello, ${name}! Welcome aboard.`
+console.log(greet("Abhinav"))
+
+//call back hell in js
+
+function getCandiees(callback) {
+    setTimeout(() => {
+        let candies = ['chocolate', 'lollipop', 'gummy bears'];
+        callback(candies);
+    }, 2000);
+}
+
+getCandiees((candies) => {
+    console.log("Candies fetched:", candies);
+});
