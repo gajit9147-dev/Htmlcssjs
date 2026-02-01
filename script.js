79a39779004ea94e432714f537b15e5f81991724
Ajeet Gupta <229371738+gajit9147-dev@users.noreply.github.com>
2026-02-02 00:16:28 +0530

    function

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-let age = prompt("Enter tour age?")
-age = Number.parseInt(age)
-const canDrive = (age) => {
-    return age >= 18 ? true : false
-}
+// let age = prompt("Enter tour age?")
+// age = Number.parseInt(age)
+// const canDrive = (age) => {
+//     return age >= 18 ? true : false
+// }
 
-if (canDrive(age)) {
-    alert("You can drive")
-} else {
-    alert("You can't drive")
+// if (canDrive(age)) {
+//     alert("You can drive")
+// } else {
+//     alert("You can't drive")
+// }
+
+//named function 
+function additon(x, y) {
+    sum = x + y
+    console.log("The sum of x and y is ", sum)
+
 }
+additon(5, 10)
+
+//anonymous function
+const sub = function(x, y) {
+    difference = x - y
+    console.log("The difference of x and y is ", difference)
+}
+sub(10, 5)
